17b24345178c19df64f4f9d8308423016cda7fe0
Vinicius Oliveira <vinicius853o@gmail.com>
2021-12-15 17:05:53 -0300

    utilizando listas e dados da api

--- consumo_servico_avancado/lib/pagina_inicial.dart
+++ consumo_servico_avancado/lib/pagina_inicial.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:consumo_servico_avancado/post.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -12,40 +12,63 @@ class PaginaInicial extends StatefulWidget {
 }
 
 class _PaginaInicialState extends State<PaginaInicial> {
-  Future<Map> _recuperarPreco() async {
-    String url = "https://blockchain.info/ticker";
-    http.Response response = await http.get(url);
-    return json.decode(response.body);
+  Future<List<Post>> _recuperarPostagens() async {
+    String _urlBase = "https://jsonplaceholder.typicode.com";
+    http.Response response = await http.get(_urlBase + "/posts");
+    var dadosJson = json.decode(response.body);
+
+    List<Post> postagens = [];
+    for (var post in dadosJson) {
+      Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
+      postagens.add(p);
+    }
+    return postagens;
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-        future: _recuperarPreco(),
-        // ignore: missing_return
-        builder: (context, snapshot) {
-          String resultado;
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-            case ConnectionState.active:
-              print('conexao active');
-              resultado = "Carregando...";
-              break;
-            case ConnectionState.none:
-            case ConnectionState.done:
-              print('conexao done');
-              if (snapshot.hasError) {
-                resultado = "erro ao carregar os dados";
-              } else {
-                double valor = snapshot.data["BRL"]["buy"];
-                resultado = valor.toString();
-              }
-              break;
-          }
-
-          return Center(
-            child: Text(resultado),
-          );
-        });
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de serviço avançado"),
+      ),
+      body: FutureBuilder<List<Post>>(
+          future: _recuperarPostagens(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+                break;
+              case ConnectionState.active:
+              case ConnectionState.done:
+                if (snapshot.hasError) {
+                  print(snapshot.error);
+                  print("Erro ao carregar a lista");
+                } else {
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, indice) {
+                      return ListTile(
+                        title: Text(snapshot.data[indice].title),
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text(snapshot.data[indice].title),
+                                content: Text(snapshot.data[indice].body),
+                              );
+                            },
+                          );
+                        },
+                      );
+                    },
+                  );
+                }
+                break;
+            }
+            return Container();
+          }),
+    );
   }
 }
