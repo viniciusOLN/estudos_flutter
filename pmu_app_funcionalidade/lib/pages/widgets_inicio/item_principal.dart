225f744045a56486c29c3867a1d77a6866088671
Vinicius Oliveira <vinicius853o@gmail.com>
2022-01-19 17:08:55 -0300

    formatando o item que aparecerá na tela principal

---
+++ pmu_app_funcionalidade/lib/pages/widgets_inicio/item_principal.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pmu_app_funcionalidade/pages/widgets_inicio/container_branco.dart';
+
+class ItemPrincipal extends StatelessWidget {
+  String titulo;
+  Icon icon;
+  Function ontap;
+  ItemPrincipal(this.ontap, this.titulo, this.icon, {Key key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: ContainerBranco(
+        ListTile(
+          title: Text(
+            titulo,
+            style: const TextStyle(
+              fontSize: 18,
+              color: Color.fromRGBO(64, 64, 64, 1),
+            ),
+          ),
+          leading: icon,
+          onTap: ontap,
+        ),
+      ),
+    );
+  }
+}
