87e7f9d2192e8a7797ebe431f22440767017d080
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-09 17:13:32 -0300

    detalhando a função do repository

--- getx_study/lib/app/data/repository/auth_repository.dart
+++ getx_study/lib/app/data/repository/auth_repository.dart
@@ -1,6 +1,12 @@
 import 'package:getx_study/app/data/model/auth_model.dart';
 import 'package:getx_study/app/data/provider/auth_provider.dart';
 
+/*
+  O repository é responsável por pegar todos os dados que a provider retorna e
+   transformá-lo para um model, dessa forma ele retorna os dados em forma de um
+   model para o controller e pode ser consumido na tela.
+ */
+
 class AuthRepository {
   final AuthApiClient apiClient = AuthApiClient();
 
