2719621e2cc1e67c9e465a70c55543f95ddaa22b
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-09 17:04:48 -0300

    adicionando novas páginas com suas bindings

---
+++ getx_study/lib/app/modules/login/login_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_study/app/modules/login/login_controller.dart';
+
+class LoginPage extends GetPage<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
