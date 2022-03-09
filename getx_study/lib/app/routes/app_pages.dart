71c4421626d269eb09cb806dc75305c10ff4d47c
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-09 17:15:55 -0300

    detalhando as rotas do app

--- getx_study/lib/app/routes/app_pages.dart
+++ getx_study/lib/app/routes/app_pages.dart
@@ -1,14 +1,21 @@
 import 'package:get/get.dart';
+import 'package:getx_study/app/modules/home/home_binding.dart';
+import 'package:getx_study/app/modules/home/home_view.dart';
 import 'package:getx_study/app/routes/app_routes.dart';
 
+/*
+  Aqui ficam todas as rotas do aplicativo, com seus nomes, para qual página direcionar
+  e se possui alguma dependencia que será injetada para não poluir a view com várias
+  instâncias.
+ */
 class AppPages {
   static const INITIAL = Routes.INITIAL;
 
   static final routes = [
     GetPage(
       name: Routes.INITIAL,
-      page: () => null,
-      binding: null,
+      page: () => HomePage(),
+      binding: HomeBinding(),
     ),
   ];
 }
