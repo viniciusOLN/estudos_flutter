dc5fba6308a73c3d6abfb54f7bc35a0209736130
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-09 17:14:25 -0300

    adicionando um controller para a home

---
+++ getx_study/lib/app/modules/home/home_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+/* 
+  O controller é a classe responsável por controlar todo o estado da view dele.
+  Ele é responsável por pegar todos os dados que a repository retorna em forma de
+  modelo da provider dentro da data.
+*/
+
+class HomeController extends GetxController {
+  void botaoClick() {}
+}
