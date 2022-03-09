65477b044d48e3efc71b53a695e4233a0c2f18ce
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-09 17:15:14 -0300

    adicionando binding para a tela de login

--- getx_study/lib/app/modules/login/login_binding.dart
+++ getx_study/lib/app/modules/login/login_binding.dart
@@ -2,6 +2,12 @@ import 'package:get/get.dart';
 
 import 'package:get/get.dart';
 
+/*
+  A binding é responsável por injetar dependências dentro da view.
+  Dessa forma a gente não precisa instancias vários controllers, repositories ou providers
+  dentro da mesma deixando um código poluído.
+ */
+
 class LoginBinding implements Bindings {
   @override
   void dependencies() {}
