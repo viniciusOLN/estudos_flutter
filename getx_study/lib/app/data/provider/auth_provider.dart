e62ceffd55fa3b53003c66e2979790c5a7aa56c6
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-09 17:13:07 -0300

    detalhando a função do provider

--- getx_study/lib/app/data/provider/auth_provider.dart
+++ getx_study/lib/app/data/provider/auth_provider.dart
@@ -1,5 +1,9 @@
 import 'package:getx_study/app/data/fake_data.dart';
 
+/*
+  O provider é a classe responsável por acessar os dados da api, nele se trata
+  o retorno e os dados repassados pela api.
+ */
 class AuthApiClient {
   Map<String, dynamic> login(String name, String password) {
     Map<String, dynamic> fakeData = FakeData.responseApiLoginUser;
