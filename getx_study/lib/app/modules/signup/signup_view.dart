76d939ce2ad138bba3a5b6ebb3d0dcd39ce01674
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-14 17:10:46 -0300

    paginas da tela de cadastro

---
+++ getx_study/lib/app/modules/signup/signup_view.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:getx_study/app/modules/signup/signup_controller.dart';
+import 'package:getx_study/app/routes/app_routes.dart';
+import 'package:getx_study/app/themes/app_theme.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class SignUpPage extends GetView<SignUpController> {
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
