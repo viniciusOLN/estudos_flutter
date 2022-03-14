c836ede0036110547e0da2e096c3e9ded122851d
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-14 17:09:22 -0300

    adicionando novas rotas

--- getx_study/lib/app/routes/app_routes.dart
+++ getx_study/lib/app/routes/app_routes.dart
@@ -3,4 +3,7 @@
 abstract class Routes {
   static const INITIAL = '/';
   static const HOMEPAGE = '/home';
+  static const LOGIN = '/login';
+  static const WELCOME = '/welcome';
+  static const SIGNUP = '/signup';
 }
