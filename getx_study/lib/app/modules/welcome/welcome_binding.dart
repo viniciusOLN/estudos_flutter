292d668d3c282599438a2508ed12764b220716e4
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-14 17:11:32 -0300

    modulos da tela de saudação

---
+++ getx_study/lib/app/modules/welcome/welcome_binding.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class WelcomeBinding implements Bindings {
+  @override
+  void dependencies() {}
+}
---
+++ getx_study/lib/app/modules/welcome/welcome_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_study/app/modules/login/login_view.dart';
+import 'package:getx_study/app/modules/signup/signup_view.dart';
+import 'package:getx_study/app/modules/welcome/welcome_controller.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:getx_study/app/themes/app_theme.dart';
+import 'package:getx_study/app/utils/widgets/rounded_button.dart';
+import 'widgets/background_widget.dart';
+
+class WelcomePage extends GetView<WelcomeController> {
+  const WelcomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Scaffold(
+      body: Background(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "BARBER APP",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: size.height * 0.05),
+              SvgPicture.asset(
+                "assets/chat.svg",
+                height: size.height * 0.45,
+              ),
+              SizedBox(height: size.height * 0.05),
+              RoundedButton(
+                text: "ENTRAR",
+                press: () => Get.toNamed('/login'),
+              ),
+              RoundedButton(
+                text: "CADASTRAR",
+                color: appThemeData.primaryColor,
+                textColor: Colors.black,
+                press: () => Get.toNamed('/signup'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
