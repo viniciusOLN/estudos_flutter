4e618eb5d916d07b819f9a622fff827b5cc2b3ec
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-14 17:08:00 -0300

    modificando a splashscreen

--- getx_study/lib/app/modules/initial/initial_view.dart
+++ getx_study/lib/app/modules/initial/initial_view.dart
@@ -1,22 +1,37 @@
+import 'package:getx_study/app/modules/home/home_view.dart';
 import 'package:getx_study/app/modules/initial/initial_controller.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:getx_study/app/routes/app_routes.dart';
+import 'package:getx_study/app/themes/app_theme.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 class InitialPage extends GetView<InitialController> {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Obx(() => Text("${controller.nome}")),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          SplashScreen(
+            seconds: 5,
+            gradientBackground: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                SplashScreenInitialColor,
+                SplashScreenFinishColor,
+              ],
+            ),
+            navigateAfterSeconds: Routes.WELCOME,
           ),
-          IconButton(
-            onPressed: () {
-              controller.atualizar();
-            },
-            icon: Icon(Icons.add),
+          Container(
+            margin: EdgeInsets.all(100),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/online-shop.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
           ),
         ],
       ),
