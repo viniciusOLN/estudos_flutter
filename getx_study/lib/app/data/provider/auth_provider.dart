a4b32a17d27888743228620aea5271760322384f
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-14 17:06:39 -0300

    modificando o provider

--- getx_study/lib/app/data/provider/auth_provider.dart
+++ getx_study/lib/app/data/provider/auth_provider.dart
@@ -7,9 +7,14 @@ import 'package:getx_study/app/data/fake_data.dart';
 class AuthApiClient {
   Map<String, dynamic> login(String name, String password) {
     Map<String, dynamic> fakeData = FakeData.responseApiLoginUser;
-    if (name != fakeData['username'] && password != fakeData['password']) {
-      throw 404;
+
+    try {
+      if (name != fakeData['username'] && password != fakeData['password']) {
+        throw 404;
+      }
+      return fakeData;
+    } catch (e) {
+      print('<provider>: error[${e}]');
     }
-    return fakeData;
   }
 }
