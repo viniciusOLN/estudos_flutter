c7d5b2f50e503a29e28f804e3fc43adae32426cb
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-14 17:08:21 -0300

    modificando a binding da login

--- getx_study/lib/app/modules/login/login_binding.dart
+++ getx_study/lib/app/modules/login/login_binding.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
 
 import 'package:get/get.dart';
+import 'package:getx_study/app/data/provider/auth_provider.dart';
+import 'package:getx_study/app/data/repository/auth_repository.dart';
+import 'package:getx_study/app/modules/login/login_controller.dart';
 
 /*
   A binding é responsável por injetar dependências dentro da view.
@@ -10,5 +13,9 @@ import 'package:get/get.dart';
 
 class LoginBinding implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.lazyPut<LoginController>(() => LoginController());
+    Get.lazyPut<AuthRepository>(() => AuthRepository());
+    Get.lazyPut<AuthApiClient>(() => AuthApiClient());
+  }
 }
