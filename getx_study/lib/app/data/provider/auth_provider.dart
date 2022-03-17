669b3e37ce8878f47164f9945610d624407b8bb1
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-17 15:17:22 -0300

    modificando o provider pra receber o tratamento adequado

--- getx_study/lib/app/data/provider/auth_provider.dart
+++ getx_study/lib/app/data/provider/auth_provider.dart
@@ -1,20 +1,22 @@
 import 'package:getx_study/app/data/fake_data.dart';
+import 'package:http/http.dart';
 
 /*
   O provider é a classe responsável por acessar os dados da api, nele se trata
   o retorno e os dados repassados pela api.
  */
 class AuthApiClient {
-  Map<String, dynamic> login(String name, String password) {
+  Future<Map<String, dynamic>> login(String name, String password) async {
     Map<String, dynamic> fakeData = FakeData.responseApiLoginUser;
 
     try {
-      if (name != fakeData['username'] && password != fakeData['password']) {
-        throw 404;
+      if (name != fakeData['user']['username'] ||
+          password != fakeData['user']['password']) {
+        return null;
       }
       return fakeData;
     } catch (e) {
-      print('<provider>: error[${e}]');
+      print('<auth provider>: ' + e);
     }
   }
 }
