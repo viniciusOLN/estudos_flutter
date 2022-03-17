5abd9ee63f7928db5a15b419455b39714de5fc72
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-17 15:19:34 -0300

    modificando controller

--- getx_study/lib/app/modules/welcome/welcome_controller.dart
+++ getx_study/lib/app/modules/welcome/welcome_controller.dart
@@ -1,3 +1,19 @@
 import 'package:get/get.dart';
+import 'package:getx_study/app/data/model/auth_model.dart';
+import 'package:getx_study/app/data/model/user_model.dart';
+import 'package:getx_study/app/data/repository/auth_repository.dart';
 
-class WelcomeController extends GetxController {}
+class WelcomeController extends GetxController {
+  AuthRepository repository = Get.put(AuthRepository());
+  Auth login;
+
+  printdata() async {
+    login = await repository.login("admin", "admin");
+    if (login == null) {
+      print(
+          'email ou senha errados ou usario nao encontrando no banco de dados');
+    } else {
+      print('esse user existe!');
+    }
+  }
+}
