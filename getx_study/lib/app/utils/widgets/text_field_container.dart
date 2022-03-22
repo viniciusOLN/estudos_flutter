b70b0308a77b911609a1ee90bbb06637099f55f4
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-22 18:08:47 -0300

    modificando a formatação do container de senha

--- getx_study/lib/app/utils/widgets/text_field_container.dart
+++ getx_study/lib/app/utils/widgets/text_field_container.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'custom_message_input_field.dart';
 
 class TextFieldContainer extends StatelessWidget {
   final Widget child;
-  const TextFieldContainer({
+  bool error;
+  final String errorMessage;
+  TextFieldContainer({
     Key key,
     this.child,
+    this.error,
+    this.errorMessage,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      width: size.width * 0.9,
-      decoration: BoxDecoration(
-        color: Get.theme.highlightColor,
-        borderRadius: BorderRadius.circular(29),
-      ),
-      child: child,
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          width: size.width * 0.9,
+          decoration: BoxDecoration(
+            color: Get.theme.highlightColor,
+            borderRadius: BorderRadius.circular(29),
+          ),
+          child: child,
+        ),
+        !error
+            ? Container()
+            : MessageErrorInput(
+                errorMessage: errorMessage,
+              ),
+      ],
     );
   }
 }
