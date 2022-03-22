c8184bea1f22f96f087d56f3c8c6b0f92cdef696
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-21 22:04:11 -0300

    modificando provider para pegar resultados da api falsa

--- getx_study/lib/app/data/provider/auth_provider.dart
+++ getx_study/lib/app/data/provider/auth_provider.dart
@@ -1,22 +1,18 @@
-import 'package:getx_study/app/data/fake_data.dart';
 import 'package:http/http.dart';
+import '../../../backend/auth/auth.dart';
 
 /*
   O provider é a classe responsável por acessar os dados da api, nele se trata
   o retorno e os dados repassados pela api.
  */
+
 class AuthApiClient {
   Future<Map<String, dynamic>> login(String name, String password) async {
-    Map<String, dynamic> fakeData = FakeData.responseApiLoginUser;
-
+    AuthAPI responseApi = AuthAPI();
     try {
-      if (name != fakeData['user']['username'] ||
-          password != fakeData['user']['password']) {
-        return null;
-      }
-      return fakeData;
+      return await responseApi.autenticateLogin(name, password);
     } catch (e) {
-      print('<auth provider>: ' + e);
+      rethrow;
     }
   }
 }
