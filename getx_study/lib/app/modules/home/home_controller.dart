3ac80827f97d87c907de655b6e8f2d6eb4bd587d
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-25 17:09:59 -0300

    adicionando função de logout

--- getx_study/lib/app/modules/home/home_controller.dart
+++ getx_study/lib/app/modules/home/home_controller.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:getx_study/app/routes/app_routes.dart';
 
 /* 
   O controller é a classe responsável por controlar todo o estado da view dele.
@@ -9,4 +11,10 @@ import 'package:get/get.dart';
 class HomeController extends GetxController {
   var count = 0.obs;
   void increment() => count++;
+  final box = GetStorage(Routes.STORAGEGET);
+
+  void clearStorageLogin() {
+    box.erase();
+    Get.offAllNamed('/welcome');
+  }
 }
