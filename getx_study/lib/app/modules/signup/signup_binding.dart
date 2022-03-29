e6466c66c3b5be16a3f694ecc07f6b6f438d92af
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-28 22:36:57 -0300

    injetando dependencias na tela de cadastro

--- getx_study/lib/app/modules/signup/signup_binding.dart
+++ getx_study/lib/app/modules/signup/signup_binding.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
+import 'package:getx_study/app/data/provider/register_provider.dart';
+import 'package:getx_study/app/data/repository/register_repository.dart';
 import 'package:getx_study/app/modules/signup/signup_controller.dart';
 
 class SignUpBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => SignUpController());
+    Get.lazyPut<RegisterRepository>(() => RegisterRepository());
+    Get.lazyPut<RegisterApiClient>(() => RegisterApiClient());
   }
 }
