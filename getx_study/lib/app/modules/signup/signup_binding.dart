8415e9021decdd99b9afd56cfe5c9b4feb902a11
Vinicius Oliveira <vinicius853o@gmail.com>
2022-03-30 21:11:24 -0300

    adicionando o authRepository nas bindings, permitindo a classe FormFields acessar o mesmo

--- getx_study/lib/app/modules/signup/signup_binding.dart
+++ getx_study/lib/app/modules/signup/signup_binding.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:getx_study/app/data/provider/register_provider.dart';
+import 'package:getx_study/app/data/repository/auth_repository.dart';
 import 'package:getx_study/app/data/repository/register_repository.dart';
 import 'package:getx_study/app/modules/signup/signup_controller.dart';
 
@@ -7,6 +8,7 @@ class SignUpBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => SignUpController());
+    Get.lazyPut<AuthRepository>(() => AuthRepository());
     Get.lazyPut<RegisterRepository>(() => RegisterRepository());
     Get.lazyPut<RegisterApiClient>(() => RegisterApiClient());
   }
