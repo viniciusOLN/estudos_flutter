4b4fe9a9f9943a7b41169b94ba06b8bc4fbb7fac
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-01 20:32:54 -0300

    adicionando bottomnavbar customizado

--- getx_study/lib/app/modules/home/home_view.dart
+++ getx_study/lib/app/modules/home/home_view.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_study/app/modules/home/widgets/bottom_navigation.dart';
 import 'home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
   @override
-  Widget build(context) => Scaffold(
-        appBar: AppBar(title: Text("counter")),
-        body: Center(
-          child: Obx(() => Text("${controller.count}")),
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Barber App'),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: controller.clearStorageLogin,
-        ),
-      );
+      ),
+      body: Column(
+        children: const <Widget>[
+          Text('Oi'),
+        ],
+      ),
+      bottomNavigationBar: BottomNavCustom(),
+    );
+  }
 }
