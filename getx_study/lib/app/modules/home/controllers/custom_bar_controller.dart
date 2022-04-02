1b1d4e12224c5bbcf2dbeef045071cae7cd0eeec
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-01 21:24:41 -0300

    adicionando função de mostrar a página tual

--- getx_study/lib/app/modules/home/controllers/custom_bar_controller.dart
+++ getx_study/lib/app/modules/home/controllers/custom_bar_controller.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_study/app/modules/home/body_home_view.dart';
 import 'package:getx_study/app/modules/home/models/navigation_item_model.dart';
+import 'package:getx_study/app/modules/profile/profile_view.dart';
+import 'package:getx_study/app/modules/search/search_view.dart';
+import '../home_controller.dart';
 
 class ControllerCustomBar extends GetxController {
+  final controllerHome = Get.put(HomeController());
+
   List<NavigationItem> listItens = [
-    NavigationItem(Icon(Icons.home), Text('Home'), Colors.deepPurpleAccent),
-    NavigationItem(Icon(Icons.search), Text('Procurar'), Colors.pinkAccent),
-    NavigationItem(Icon(Icons.person), Text('Profile'), Colors.cyan[800]),
+    NavigationItem(
+      icon: const Icon(Icons.home),
+      title: const Text('Home'),
+      color: Colors.deepPurpleAccent,
+      page: const HomePageBody(),
+    ),
+    NavigationItem(
+      icon: const Icon(Icons.search),
+      title: const Text('Procurar'),
+      color: Colors.pinkAccent,
+      page: const Searchpage(),
+    ),
+    NavigationItem(
+      icon: const Icon(Icons.person),
+      title: const Text('Profile'),
+      color: Colors.cyan[800],
+      page: const ProfilePage(),
+    ),
   ];
 
   int selectedItem = 0;
@@ -14,5 +35,10 @@ class ControllerCustomBar extends GetxController {
   updateBar(itemIndex) {
     selectedItem = itemIndex;
     update(['customBar']);
+    update(['currentPage']);
+  }
+
+  Widget currentPage() {
+    return listItens[selectedItem].page;
   }
 }
