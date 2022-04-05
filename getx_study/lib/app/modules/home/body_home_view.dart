9d8945bdc6999693e9c37f8e84da663f49991535
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-05 18:12:30 -0300

    adicionando listview que vai conter os serviços

--- getx_study/lib/app/modules/home/body_home_view.dart
+++ getx_study/lib/app/modules/home/body_home_view.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_study/app/data/model/schedule_model.dart';
+import 'package:getx_study/app/modules/home/home_controller.dart';
 
 class HomePageBody extends StatelessWidget {
   const HomePageBody({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home'),
+    return GetBuilder<HomeController>(
+      id: 'listSchedules',
+      builder: (_) {
+        return ListView.builder(
+          itemCount: _.listSchedules.length,
+          itemBuilder: (context, indice) {
+            Schedule listItemSchedule = _.listSchedules[indice];
+            return ListTile(
+              title: Text(listItemSchedule.service.name),
+              subtitle: Text(listItemSchedule.schedulingDate),
+            );
+          },
+        );
+      },
     );
   }
 }
