58883e59354a6341af777c19b596fecf86da6207
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-05 18:14:18 -0300

    dados em novo formato

--- getx_study/lib/backend/fake_db.dart
+++ getx_study/lib/backend/fake_db.dart
@@ -23,4 +23,45 @@ class FakeData {
       "expires_in": 3600,
     },
   ];
+
+  static List<Map<String, dynamic>> responseSchedules = [
+    {
+      "id": 1,
+      "scheduling_date": "2001-1109T3:4",
+      "user_id": 1,
+      "employee_id": 1,
+      "service_id": 1,
+      "employee": {
+        "id": 1,
+        "first_name": "José",
+        "company_id": 1,
+        "user_id": 1
+      },
+      "service": {
+        "id": 1,
+        "name": "corte de cabelo",
+        "cost": 25,
+        "employee_id": 1
+      },
+    },
+    {
+      "id": 1,
+      "scheduling_date": "2001-1109T3:4",
+      "user_id": 1,
+      "employee_id": 1,
+      "service_id": 1,
+      "employee": {
+        "id": 1,
+        "first_name": "José",
+        "company_id": 1,
+        "user_id": 1
+      },
+      "service": {
+        "id": 1,
+        "name": "corte de cabelo",
+        "cost": 25,
+        "employee_id": 1
+      },
+    },
+  ];
 }
