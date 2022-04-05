849577372c65a464c8840ed65ea6a70c32688c79
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-05 18:14:00 -0300

    adicionando função para receber os dados da fake api

--- getx_study/lib/app/modules/home/home_controller.dart
+++ getx_study/lib/app/modules/home/home_controller.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:getx_study/app/data/model/schedule_model.dart';
+import 'package:getx_study/app/data/repository/schedule_repository.dart';
 import 'package:getx_study/app/routes/app_routes.dart';
+import 'package:getx_study/app/utils/widgets/dialog_notification.dart';
 
 /* 
   O controller é a classe responsável por controlar todo o estado da view dele.
@@ -10,6 +13,30 @@ import 'package:getx_study/app/routes/app_routes.dart';
 
 class HomeController extends GetxController {
   final box = GetStorage(Routes.STORAGEGET);
+  final repository = Get.find<ScheduleRepository>();
+  List<Schedule> listSchedules;
+
+  @override
+  void onInit() {
+    loadData();
+    super.onInit();
+  }
+
+  void loadData() async {
+    await repository.getAll().then(
+      (value) {
+        listSchedules = value;
+      },
+    ).onError(
+      (error, stackTrace) {
+        return CustomNotification.showNotification(
+          "ERRO",
+          "Erro de conexão, tente novamente!",
+        );
+      },
+    );
+    update(['listSchedules']);
+  }
 
   void clearStorageLogin() {
     box.erase();
