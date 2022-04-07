aad4d2f6d9e0dfc3b32eb4c3663706873d16f3dd
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-07 18:32:20 -0300

    adicionando o componente de cardService

--- getx_study/lib/app/modules/home/body_home_view.dart
+++ getx_study/lib/app/modules/home/body_home_view.dart
@@ -3,25 +3,45 @@ import 'package:get/get.dart';
 import 'package:getx_study/app/data/model/schedule_model.dart';
 import 'package:getx_study/app/modules/home/home_controller.dart';
 
+import 'widgets/card_service.dart';
+
 class HomePageBody extends StatelessWidget {
   const HomePageBody({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<HomeController>(
-      id: 'listSchedules',
-      builder: (_) {
-        return ListView.builder(
-          itemCount: _.listSchedules.length,
-          itemBuilder: (context, indice) {
-            Schedule listItemSchedule = _.listSchedules[indice];
-            return ListTile(
-              title: Text(listItemSchedule.service.name),
-              subtitle: Text(listItemSchedule.schedulingDate),
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Serviços Marcados'),
+          ),
+        ),
+        GetBuilder<HomeController>(
+          id: 'listSchedules',
+          builder: (_) {
+            return Expanded(
+              child: ListView.builder(
+                itemCount: _.listSchedules.length,
+                itemBuilder: (context, indice) {
+                  Schedule listItemSchedule = _.listSchedules[indice];
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: CardService(
+                      categoryService: listItemSchedule.service.name,
+                      titleService: listItemSchedule.service.name,
+                      priceService: listItemSchedule.service.cost,
+                      dateService: listItemSchedule.schedulingDate,
+                    ),
+                  );
+                },
+              ),
             );
           },
-        );
-      },
+        )
+      ],
     );
   }
 }
