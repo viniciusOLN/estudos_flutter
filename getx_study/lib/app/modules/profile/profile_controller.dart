affe32320980337ac90409d336b39bf4f104d2fc
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-08 22:41:24 -0300

    modificando controller do perfil do usuário

--- getx_study/lib/app/modules/profile/profile_controller.dart
+++ getx_study/lib/app/modules/profile/profile_controller.dart
@@ -1,3 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_study/app/modules/home/home_controller.dart';
+import 'models/item_menu_profile.dart';
 
-class ProfileController extends GetxController {}
+class ProfileController extends GetxController {
+  final controllerHome = Get.find<HomeController>();
+
+  String getUserFullName() {
+    return controllerHome.auth.user.fullname;
+  }
+
+  List<ItemProfileMenu> listItemMenu = [
+    ItemProfileMenu(
+      title: 'Configurações',
+      subtitle: 'Definição de privacidade',
+      leading: 'assets/settings_icon.png',
+      trailing: Icon(Icons.chevron_right),
+    ),
+    ItemProfileMenu(
+      title: 'Ajuda & Suporte',
+      subtitle: 'Centro de ajuda e suporte',
+      leading: 'assets/support.png',
+      trailing: Icon(Icons.chevron_right),
+    ),
+    ItemProfileMenu(
+      title: 'Sair',
+      subtitle: 'Retornar para a página de login',
+      leading: 'assets/faq.png',
+      trailing: Icon(Icons.chevron_right),
+    ),
+  ];
+}
