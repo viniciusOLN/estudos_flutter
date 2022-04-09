1c41d4033689deb04b6bb78998f43546fbaf85f9
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-08 22:43:28 -0300

    arquivos de model internos de widgets

---
+++ getx_study/lib/app/modules/profile/models/item_menu_profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItemProfileMenu {
+  String title;
+  String subtitle;
+  String leading;
+  Icon trailing;
+
+  ItemProfileMenu({this.title, this.subtitle, this.leading, this.trailing});
+}
