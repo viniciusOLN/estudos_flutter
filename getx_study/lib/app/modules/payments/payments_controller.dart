f706070f7ca5398a262f96d9c87e1c263617d53b
Vinicius Oliveira <vinicius853o@gmail.com>
2022-04-08 22:43:47 -0300

    arquivos da página de pagamentos

---
+++ getx_study/lib/app/modules/payments/payments_controller.dart
@@ -0,0 +1,3 @@
+import 'package:get/get.dart';
+
+class PaymentsController extends GetxController {}
---
+++ getx_study/lib/app/modules/payments/payments_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'payments_controller.dart';
+
+class PaymentsPage extends GetView<PaymentsController> {
+  const PaymentsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
