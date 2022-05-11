84c94453c02a92c1fcbb9b1df793e7cd346703ad
vinicius <vinicius853o@gmail.com>
2022-05-10 21:40:58 -0300

    adicionando as funções de configuração dos pins dentro do maps(fix later)

--- getx_study/lib/app/modules/search/search_controller.dart
+++ getx_study/lib/app/modules/search/search_controller.dart
@@ -1,3 +1,30 @@
+import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class SearchController extends GetxController {}
+class SearchController extends GetxController {
+  Rx<LatLng> center = const LatLng(0, 0).obs;
+  Completer<GoogleMapController> gmapController = Completer();
+  LatLng lastMapPosition;
+  Position currentLocation;
+
+  Future<Position> locateUser() async {
+    return Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+  }
+
+  getUserLocation() async {
+    currentLocation = await locateUser();
+    center.value = LatLng(currentLocation.latitude, currentLocation.longitude);
+  }
+
+  void onMapCreated(GoogleMapController controller) {
+    gmapController.complete(controller);
+  }
+
+  void onCameraMove(CameraPosition position) {
+    lastMapPosition = position.target;
+  }
+}
