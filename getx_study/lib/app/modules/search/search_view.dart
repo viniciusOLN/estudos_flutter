5dd1ab4b9605c10487a46cf614970c2c4f5ca443
vinicius <vinicius853o@gmail.com>
2022-05-10 21:40:31 -0300

    adicionando o google maps na tela

--- getx_study/lib/app/modules/search/search_view.dart
+++ getx_study/lib/app/modules/search/search_view.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_study/app/modules/search/search_controller.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Searchpage extends GetView<SearchController> {
   const Searchpage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('search'),
+    return Center(
+      child: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: controller.center.value,
+          zoom: 14.0,
+        ),
+        onMapCreated: controller.onMapCreated,
+        zoomGesturesEnabled: true,
+        onCameraMove: controller.onCameraMove,
+        myLocationEnabled: true,
+        compassEnabled: false,
+        myLocationButtonEnabled: true,
+        mapToolbarEnabled: false,
+      ),
     );
   }
 }
